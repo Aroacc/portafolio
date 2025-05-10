58339d76a668d665f42a92a3ae386ae4f56cff30
aroaclavijo <aroa.clavijo@i3television.es>
2025-05-10 19:26:34 +0200

    feat(header)add carousel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import '../App.css';
+import React, { useState } from 'react';
 import htmlIcon from '../assets/icons/html.png';
 import cssIcon from '../assets/icons/css.png';
 import jsIcon from '../assets/icons/js.png';
@@ -6,27 +6,64 @@ import reactIcon from '../assets/icons/react.png';
 import gitIcon from '../assets/icons/git.png';
 import gitlabIcon from '../assets/icons/gitlab.png';
 import nodeIcon from '../assets/icons/node.png';
+import About from './About';
+import Projects from './Projects';
+import '../index.css';
 
 function Header() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const presentacion = (
+    <div className="slide-content">
+      <img src="/foto-aroa.jpg" alt="Foto de Aroa" className="photo" />
+      <div className="header-text">
+        <h1>Aroa Clavijo Caravante</h1>
+        <h2 className="h1">Desarrolladora Frontend</h2>
+        <div className="tech-icons">
+          <img src={htmlIcon} alt="HTML" />
+          <img src={cssIcon} alt="CSS" />
+          <img src={jsIcon} alt="JavaScript" />
+          <img src={reactIcon} alt="React" />
+          <img src={gitIcon} alt="Git" />
+          <img src={gitlabIcon} alt="GitLab" />
+          <img src={nodeIcon} alt="Node.js" />
+        </div>
+      </div>
+    </div>
+  );
+
+  const slides = [
+    presentacion,
+    <About />,
+    <Projects />,
+   
+  ];
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev + 1) % slides.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev === 0 ? slides.length - 1 : prev - 1));
+  };
+
   return (
     <header className="header" id="inicio">
       <div className="header-content">
-        <img src="/foto-aroa.jpg" alt="Foto de Aroa" className="photo" />
-
-        <div className="header-text">
-          <h1>Aroa Clavijo Caravante</h1>
-          <h2 className="h1">Desarrolladora Frontend</h2>
-
-          <div className="tech-icons">
-            <img src={htmlIcon} alt="HTML" />
-            <img src={cssIcon} alt="CSS" />
-            <img src={jsIcon} alt="JavaScript" />
-            <img src={reactIcon} alt="React" />
-            <img src={gitIcon} alt="Git" />
-            <img src={gitlabIcon} alt="GitLab" />
-            <img src={nodeIcon} alt="Node.js" />
-          </div>
-        </div>
+        <button className="carousel-btn prev-btn" onClick={prevSlide}>‹</button>
+        <div className="carousel-wrapper">
+  <div
+    className="carousel-track"
+    style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+  >
+    {slides.map((slide, index) => (
+      <div className="carousel-slide" key={index}>
+        {slide}
+      </div>
+    ))}
+  </div>
+</div>
+        <button className="carousel-btn next-btn" onClick={nextSlide}>›</button>
       </div>
     </header>
   );
