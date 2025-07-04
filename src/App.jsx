b86eaef16f5f6b075f45a331b56ba6cc0f113999
aroaclavijo <aroa.clavijo@i3television.es>
2025-07-04 19:30:50 +0200

    fix(changes) Portfolio changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import Contact from './components/Contact/Contact';
 import Navbar from './components/Navbar/Navbar';
+import './components/footer/footer.css';
+import './components/About/About.css';
+import './components/Projects/Projects.css';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -11,35 +14,6 @@ function App() {
   };
 
   useEffect(() => {
-    const starContainer = document.querySelector('.star-container');
-
-    const createStar = () => {
-      const star = document.createElement('div');
-      star.classList.add('star');
-      star.style.left = `${Math.random() * window.innerWidth}px`;
-      star.style.top = `${Math.random() * window.innerHeight}px`;
-      starContainer.appendChild(star);
-
-      // Animar la estrella
-      setTimeout(() => {
-        star.style.transform = `translateX(${Math.random() * 800}px) translateY(${Math.random() * 800}px)`;
-        star.style.opacity = 0;
-      }, 0);
-
-      // Eliminar la estrella después de que termine la animación
-      setTimeout(() => {
-        star.remove();
-      }, 2000); // Duración de la animación
-    };
-
-    const interval = setInterval(createStar, 300); // Crea una estrella cada 300ms
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  useEffect(() => {
     document.body.classList.toggle('dark-mode', darkMode);
   }, [darkMode]);
 
@@ -47,9 +21,6 @@ function App() {
     <div className={darkMode ? 'dark-mode' : ''}>
       {/* Barra de navegación */}
       <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-
-      {/* Contenedor de estrellas fugaces */}
-      <div className="star-container"></div>
 
       {/* Contenido principal */}
       <Header />
